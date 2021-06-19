d038d905edbd3d68b13cde12d659030d6c6a71d5
qeiray <62768543+AndrewPA04@users.noreply.github.com>
2021-06-19 21:25:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+print("0:Exit\n")
+print("1:Binary to decimal\n")
+print("2:Decimal to binary\n")
+print("3:Hexadecimal to binary\n")
+print("4:Binary to text\n")
+print("5:Text to binary\n\n\n")
+
+def binToDec(binary):
+	decimal = 0
+	exponent = 0
+	reversedBinary = binary[::-1]
+	for number in reversedBinary:
+		if(number=="1"):
+			decimal += 2**exponent
+			exponent+= 1
+		elif(number=="0"):
+			exponent+= 1
+	return decimal		
+		
+	
+def decToBin(decimal):
+	binary = str(bin(decimal))
+	formattedBinary = binary[2:]
+	return formattedBinary
+
+def hexToBin(hexadecimal):
+	return format(int(hexadecimal, 16), "040b")
+	
+def binaryToText(binaryUser):
+	binaryArray = binary.split()
+	decimal = 0
+	exponent = 0
+	text = ""
+	for binary in binaryArray:
+		for number in binary:
+			if(number=="1"):
+				decimal += 2**exponent
+				exponent+= 1
+			elif(number=="0"):
+				exponent+= 1
+		text += chr(decimal)
+	return text
+	
+
+while(True):
+	option = int(input("Pick an option: "))
+	if(option==0):
+		break
+	elif(option==1):
+		userBinary = str(input("Enter binary value: "))
+		print(binToDec(userBinary))
+	elif(option==2):
+		userDecimal = int(input("Enter decimal value: "))
+		print(decToBin(userDecimal))
+    	
